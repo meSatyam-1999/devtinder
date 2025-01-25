357666597dedab01e939a930150781a3978ddf1f
Satyam Singh <mesatyamsingh1999@gmail.com>
2025-01-25 21:47:23 +0530

    intial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+#Dev Tinder
+
+##Tasks to follow - 
+
+- Created a vite + react application
+- Remove unecessay code & run the app with demo data.
+- Installing tailwind in our project. (v.4)
+- DaisyUI installation & configuration (v.5)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Hello From DevTinder UI</h1>
+      <button className="btn btn-soft btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
